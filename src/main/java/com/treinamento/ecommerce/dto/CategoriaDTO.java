61122cd3977f66f3b9e2203e98a3d9d83a746e9d
wesleyroberts <wesley.roberts@liferay.com>
2022-03-05 18:53:14 -0300

    create Categoria DTO

---
+++ src/main/java/com/treinamento/ecommerce/dto/CategoriaDTO.java
@@ -0,0 +1,41 @@
+package com.treinamento.ecommerce.dto;
+
+import com.treinamento.ecommerce.domain.Categoria;
+import com.treinamento.ecommerce.domain.Produto;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaDTO implements Serializable {
+    private static final long serialVersionUID= 1L;
+
+    private Long id;
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Length(min=5, max=80, message= "O tamanho dever ser entre 5 e 80 caracteres")
+    private String nome;
+
+    public CategoriaDTO(){}
+
+    public CategoriaDTO(String nome){
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
