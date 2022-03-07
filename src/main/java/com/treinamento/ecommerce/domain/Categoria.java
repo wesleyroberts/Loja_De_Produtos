baeb30d5ef43fcadad68518ee30e7fa3de923b30
wesleyroberts <wesley.roberts@liferay.com>
2022-03-07 12:39:24 -0300

    criado produtos get e set metodos

--- src/main/java/com/treinamento/ecommerce/domain/Categoria.java
+++ src/main/java/com/treinamento/ecommerce/domain/Categoria.java
@@ -45,6 +45,14 @@ public class Categoria implements Serializable {
         this.nome = nome;
     }
 
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/main/java/com/treinamento/ecommerce/exception/CategoriaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.treinamento.ecommerce.exception;
-
-public class CategoriaNotFoundException extends RuntimeException{
-    public CategoriaNotFoundException() {
-        super("Entidade nao encontrada");
-    }
-}
