3d034e88d36e2148672ba7509dad43a627f14b62
wesleyroberts <wesley.roberts@liferay.com>
2022-03-07 13:47:59 -0300

    create Resource Exception

---
+++ src/main/java/com/treinamento/ecommerce/exception/resourceException/ResourceExecptionHandler.java
@@ -0,0 +1,24 @@
+package com.treinamento.ecommerce.exception.resourceException;
+import com.treinamento.ecommerce.exception.serviceException.ObjectNotFoundException;
+import com.treinamento.ecommerce.exception.serviceException.DataIntegrityException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ResourceExecptionHandler{
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrityException(DataIntegrityException e, HttpServletRequest request) {
+
+        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+}
