5dc478d2bca208b38d58481db27a250a46e1215a
wesleyroberts <wesley.roberts@liferay.com>
2022-03-15 21:40:00 -0300

    criado EstadoDTO

---
+++ src/main/java/com/treinamento/ecommerce/dto/EstadoDTO.java
@@ -0,0 +1,16 @@
+package com.treinamento.ecommerce.dto;
+
+public class EstadoDTO {
+
+    private String nome;
+
+    public EstadoDTO(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String name) {
+        this.nome = name;
+    }
+}
